2b8ac7af58de807cc885c036c08156806ff29a82
Karl Grasegger <51945878+grasegger@users.noreply.github.com>
2020-08-31 20:38:58 +0200

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/style.css");
+    eleventyConfig.addWatchTarget('src/style.css');
+    return {
+        dir: {
+            input: "src",
+            output: "dist",
+            includes: "_includes"
+        }
+    }
+};
